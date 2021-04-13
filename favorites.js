155b730119dca6c5a876f67b9ac898afd0a88f34
lguadarrama770 <134498@students.nccvt.k12.de.us>
2021-04-13 15:21:43 -0400

    favorites and explore colleges page

--- favorites.js
+++ favorites.js
@@ -1,19 +1,19 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+// /* When the user clicks on the button, 
+// toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   };
   
-  // Closes the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbutton')) {
-      let dropdowns = document.getElementsByClassName("dropdown-favorites");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+// //EventListener to show dropdown
+//   favorites.addEventListener('click', (event) =>{
+//     if (!event.target.matches('.dropbutton')) {
+//       let dropdowns = document.getElementsByClassName("dropdown-favorites");
+//       let i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         let openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   });
